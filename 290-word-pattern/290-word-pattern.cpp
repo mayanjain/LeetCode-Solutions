841d4f61bc756cf49c5b808bc9b77194ccdffec9
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-01-17 23:19:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (50.50%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool wordPattern(string p, string s) {
-        unordered_map<char,string> mp;
-        unordered_map<string,char> up;
+        unordered_map<char,int> mp;
+        unordered_map<string,int> up;
         stringstream ss(s);
         string word;
         int i=0;
         while(ss>>word){
-            if(i==p.size())return 0;
-            if(mp.count(p[i]) && mp[p[i]]!=word)return false;
-            if(up.count(word) && up[word]!=p[i])return false;
-            up[word]=p[i];
-            mp[p[i++]]=word;
-            // cout<<word;
+            if(i==p.size() || mp[p[i]]!=up[word])return 0;
+            mp[p[i]]=i+1;up[word]=i+1;i++;
         }
         return i==p.size();
     }
