3aee588f9284cf894f89ab63808132e606a62455
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-01-29 23:48:58 +0530

    Time: 184 ms (39.22%), Space: 63.3 MB (67.11%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& ht) {
+        stack<int> st;
+        int ans=0;
+        ht.push_back(0);
+        for(int i=0 ; i<ht.size() ; i++){
+            while(!st.empty() && ht[st.top()]>ht[i]){
+                int h=ht[st.top()];st.pop();
+                int l=st.empty()?-1:st.top();
+                ans=max(ans,(i-l-1)*h);
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
