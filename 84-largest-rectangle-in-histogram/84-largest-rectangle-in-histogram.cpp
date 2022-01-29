b9eea98d8a98ed174bcbcf90696f86a6d25f3fef
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-01-29 23:50:17 +0530

    Time: 176 ms (41.91%), Space: 61.6 MB (81.22%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -5,7 +5,7 @@ public:
         int ans=0;
         ht.push_back(0);
         for(int i=0 ; i<ht.size() ; i++){
-            while(!st.empty() && ht[st.top()]>ht[i]){
+            while(!st.empty() && ht[st.top()]>=ht[i]){
                 int h=ht[st.top()];st.pop();
                 int l=st.empty()?-1:st.top();
                 ans=max(ans,(i-l-1)*h);
