67f239b11b539014d09cc83ac4fe1cf829070928
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-02-07 00:57:11 +0530

    Time: 6 ms (25.00%), Space: 6.1 MB (75.00%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -6,15 +6,23 @@ public:
             neg=1;
             num=-num;
         }
-        string s=to_string(num);
-        sort(s.begin(),s.end());
-        if(neg){
-            reverse(s.begin(),s.end());
-            return -stoll(s);
+        vector<int> v;
+        while(num){
+            v.push_back(num%10);
+            num/=10;
         }
-        int i=0;
-        while(i<s.size() && s[i]=='0')i++;
-        if(i!=s.size())swap(s[0],s[i]);
-        return stoll(s);
+        sort(v.begin(),v.end());
+        if(neg)reverse(v.begin(),v.end());
+        else{
+            int i=0;
+            while(i<v.size() && v[i]==0)i++;
+            if(i<v.size())swap(v[i],v[0]);
+        }
+        long long ans=0;
+        for(auto& i:v){
+            ans=ans*10+i;
+        }
+        if(neg)ans=-ans;
+        return ans;
     }
 };
