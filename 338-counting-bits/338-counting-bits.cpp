132a59f8434862bdf3bc1a788d945542e1741343
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-03-01 23:41:21 +0530

    Time: 3 ms (95.58%), Space: 8.6 MB (21.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> v;
-        for(int i=0 ; i<=n ; i++){
-            int j=i,c=0;
-            while(j){
-                if((j&1))c++;
-                j>>=1;
-            }
-            v.push_back(c);
+        vector<int> v{0};
+        if(n==0)return v;
+        v.push_back(1);
+        for(int i=2 ; i<=n ; i++){
+            if(i%2)v.push_back(v[i/2]+1);
+            else v.push_back(v[i/2]);
         }
         return v;
     }
