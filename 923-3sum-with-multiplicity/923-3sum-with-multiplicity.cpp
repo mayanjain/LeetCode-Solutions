5b4481558ef63a9f6955ebcf26d778e05a4ede8a
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-04-06 18:41:39 +0530

    Time: 1543 ms (6.47%), Space: 10.8 MB (32.99%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,35 +1,12 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        long n=arr.size(),ans=0;
-        sort(arr.begin(),arr.end());
-        for(int i=0 ; i<n ; i++){
-            int l=i+1,h=n-1;
-            while(l<h){
-                long sum=arr[i]+arr[l]+arr[h];
-                if(sum==target){
-                    if(arr[l]==arr[h]){
-                        long k=h-l;
-                        ans+=(k*(k+1))/2;
-                        break;
-                    }
-                    else{
-                    long first=1,second=1;
-                        while(arr[l]==arr[l+1]){
-                            l++;
-                            first++;
-                        }
-                        while(arr[h]==arr[h-1]){
-                            second++;
-                            h--;
-                        }
-                        l++;h--;
-                        ans+=first*second;
-                    }
-                }
-                else if(sum<target)l++;
-                else h--;
-                ans%=int(1e9+7);
+        int ans=0;
+        unordered_map<int,int> mp;
+        for(int i=0 ; i<arr.size() ; i++){
+            ans+=mp[target-arr[i]];
+            for(int j=0 ; j<i ; j++){
+                mp[arr[i]+arr[j]]++;
             }
             ans%=int(1e9+7);
         }
