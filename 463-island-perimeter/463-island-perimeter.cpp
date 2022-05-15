dbd47d6cf1f3b256315fe7b4dc362a3fb9158c9b
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-05-16 01:26:22 +0530

    Time: 133 ms (68.87%), Space: 96.2 MB (58.22%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int ans=0,n=grid.size(),m=grid[0].size();
-        vector<vector<int>> v(n+2,vector<int>(m+2));
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=m ; j++){
-                v[i][j]=grid[i-1][j-1];
-            }
-        }
-        for(int i=1 ; i<=n ; i++){
-            for(int j=1 ; j<=m ; j++){
+    int islandPerimeter(vector<vector<int>>& v) {
+        int ans=0,n=v.size(),m=v[0].size();
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
                 if(v[i][j]){
-                    if(!v[i-1][j])ans++;
-                    if(!v[i][j-1])ans++;
-                    if(!v[i+1][j])ans++;
-                    if(!v[i][j+1])ans++;
+                    if(i==0 || !v[i-1][j])ans++;
+                    if(j==0 || !v[i][j-1])ans++;
+                    if(i==n-1 || !v[i+1][j])ans++;
+                    if(j==m-1 || !v[i][j+1])ans++;
                 }
             }
         }
