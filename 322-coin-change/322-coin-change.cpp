37577925b556cafd7d9949e9696a0bb1ef057f0c
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-05-21 11:23:24 +0530

    Time: 110 ms (57.24%), Space: 14.5 MB (54.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
         if(!amount)return 0;
         vector<int> dp(amount+1,amount+1);
         dp[0]=0;
@@ -9,6 +10,7 @@ public:
                 if(c<=i){
                     dp[i]=min(dp[i],dp[i-c]+1);
                 }
+                else break;
             }
         }
         return dp[amount]>amount?-1:dp[amount];
