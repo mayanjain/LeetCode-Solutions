839678524dac738d68ad1ebc0fcd8bdec830ca9c
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-06-20 10:10:12 +0530

    Time: 85 ms (65.84%), Space: 17.3 MB (83.66%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
-        sort(words.begin(),words.end(),[](string& s,string& t){return s.size()>t.size();});
-        unordered_set<string> st;
+        unordered_set<string> st(words.begin(),words.end());
         int ans=0;
         for(auto& i:words){
-            if(!st.count(i)){
-                for(int j=0 ; j<i.size(); j++){
-                    st.insert(i.substr(j));
-                }
-                ans+=i.size()+1;
+            for(int j=1 ; j<i.size(); j++){
+                st.erase(i.substr(j));
             }
         }
+        for(auto& i:st)ans+=i.size()+1;
         return ans;
     }
 };
