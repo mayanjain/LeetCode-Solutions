c6f6fa0cf108a30d855c6b79b7776ab9b4b0a9ac
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-07-18 22:17:45 +0530

    Time: 38 ms (41.13%), Space: 14.5 MB (33.98%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(vector<vector<int>>& v,int cur,set<int>& st,vector<bool>& done){
+        if(done[cur])return true;
+        if(st.count(cur))return false;
+        st.insert(cur);
+        for(auto& i:v[cur]){
+            if(!solve(v,i,st,done))return false;
+        }
+        done[cur]=1;
+        return true;
+    }
+    
+    bool canFinish(int total, vector<vector<int>>& req) {
+        vector<vector<int>> v(total);
+        for(auto& i:req){
+            v[i[0]].push_back(i[1]);
+        }
+        vector<bool> done(total);
+        for(int i=0 ; i<total ; i++){
+            set<int> st;
+            if(!solve(v,i,st,done))return false;
+        }
+        return true;
+    }
+};
