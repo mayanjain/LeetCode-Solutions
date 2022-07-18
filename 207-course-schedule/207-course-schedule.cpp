e1d4a9a07871a1eeea8ef7dce1f318780abc533c
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-07-19 01:08:16 +0530

    Time: 29 ms (67.95%), Space: 13.4 MB (75.23%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    bool solve(vector<vector<int>>& v,int cur,unordered_set<int>& st,vector<bool>& done){
-        if(done[cur])return true;
-        if(st.count(cur))return false;
-        st.insert(cur);
-        for(auto& i:v[cur]){
-            if(!solve(v,i,st,done))return false;
-        }
-        done[cur]=1;
-        return true;
-    }
-    
     bool canFinish(int total, vector<vector<int>>& req) {
         vector<vector<int>> v(total);
+        vector<int> degree(total),done(total);
         for(auto& i:req){
-            v[i[0]].push_back(i[1]);
+            v[i[1]].push_back(i[0]);
+            degree[i[0]]++;
         }
-        vector<bool> done(total);
+        queue<int> q;
         for(int i=0 ; i<total ; i++){
-            unordered_set<int> st;
-            if(!solve(v,i,st,done))return false;
+            if(!degree[i]){
+                q.push(i);
+                done[i]=1;
+            }
+        }
+        while(q.size()){
+            for(int i=q.size() ; i>0 ; i--){
+                auto cur=q.front();
+                q.pop();
+                for(auto& i:v[cur]){
+                    if(--degree[i]==0){
+                        q.push(i);
+                        done[i]++;
+                    }
+                }
+            }
+        }
+        for(auto& i:done){
+            if(!i)return false;
         }
         return true;
     }
