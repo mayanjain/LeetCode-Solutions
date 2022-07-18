0f2a8650f17d8121b1e0ccfd6af54ff32a44833d
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-07-19 02:53:15 +0530

    Time: 19 ms (58.64%), Space: 9 MB (38.38%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int dp[501][501];
-    
-    int solve(string& word1,string& word2,int x,int y){
+    int solve(string& word1,string& word2,int x,int y,vector<vector<int>>& dp){
         int sz1=word1.size(),sz2=word2.size();
         if(y==sz2)return sz1-x;
         if(x==sz1)return sz2-y;
-        if(word1[x]==word2[y])return solve(word1,word2,x+1,y+1);
+        if(word1[x]==word2[y])return solve(word1,word2,x+1,y+1,dp);
         if(dp[x][y])return dp[x][y];
-        return dp[x][y]=min({solve(word1,word2,x+1,y),solve(word1,word2,x+1,y+1),solve(word1,word2,x,y+1)})+1;
+        return dp[x][y]=min({solve(word1,word2,x+1,y,dp),solve(word1,word2,x+1,y+1,dp),solve(word1,word2,x,y+1,dp)})+1;
     }
     
     int minDistance(string word1, string word2) {
-        return solve(word1,word2,0,0);
+        vector<vector<int>> dp(word1.size(),vector<int>(word2.size()));
+        return solve(word1,word2,0,0,dp);
     }
 };
