1258b6c95dcdc50d18db0d4d83e7e079372dcdef
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-07-25 02:59:32 +0530

    Time: 1350 ms (33.33%), Space: 166 MB (33.33%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,40 @@
+#include <type_traits>
+bool cmp(pair<string,int> a, pair<string,int> b) { return a.second!=b.second?a.second>b.second:a.first<b.first;};
+using Cmp = std::integral_constant<decltype(&cmp), &cmp>;
+class FoodRatings {
+    static bool cmp(pair<string,int> a, pair<string,int> b) { return a.second!=b.second?a.second>b.second:a.first<b.first;};
+    
+    
+    map<string,set<pair<string,int>,Cmp>> menu;
+    map<string,string> type;
+    map<string,int> rate;
+    
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        for(int i=0 ; i<foods.size() ; i++){
+            menu[cuisines[i]].insert({foods[i],ratings[i]});
+            type[foods[i]]=cuisines[i];
+            rate[foods[i]]=ratings[i];
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        auto it=menu[type[food]].find({food,rate[food]});
+        menu[type[food]].erase(it);
+        menu[type[food]].insert({food,newRating});
+        rate[food]=newRating;
+    }
+    
+    string highestRated(string cuisine) {
+        auto it=*menu[cuisine].begin();
+        // cout<<it.second;
+        return it.first;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
