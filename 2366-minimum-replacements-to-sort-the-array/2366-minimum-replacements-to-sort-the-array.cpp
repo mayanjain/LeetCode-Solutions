3dae851e3e4a3a05786f690f5998e42851f3b289
Mayan Jain <85707839+mayanjain@users.noreply.github.com>
2022-08-19 22:53:07 +0530

    Time: 181 ms (54.74%), Space: 54.7 MB (14.37%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -3,20 +3,12 @@ public:
     long long minimumReplacement(vector<int>& nums) {
         long long ans=0,n=nums.size();
         for(int i=n-2 ; i>=0 ; i--){
-            if(nums[i]>nums[i+1]){
-                int cur=(nums[i]/nums[i+1]),z=nums[i]%nums[i+1];
-                nums[i+1]--;
-                while(nums[i+1] && nums[i]/nums[i+1]==cur && z){
-                    nums[i+1]--;
-                }
-                nums[i+1]++;
-                ans+=cur-1;
-                if(nums[i]%nums[i+1]){
-                    ans++;
-                    nums[i]=nums[i+1]-1;
-                }
-                else nums[i]=nums[i+1];
+            int cur=nums[i]/nums[i+1];
+            if(nums[i]%nums[i+1]){
+                cur++;
             }
+            nums[i]=nums[i]/cur;
+            ans+=cur-1;
         }
         return ans;
     }
